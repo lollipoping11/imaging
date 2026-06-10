c8a487eecf7a3edc879b7bbb44ef6a152bdd28d6
lollipoping11 <120643604+lollipoping11@users.noreply.github.com>
2026-06-10 20:02:20 +0200

    trying dif threshold

--- src/main.go
+++ src/main.go
@@ -252,7 +252,7 @@ func run(service roverlib.Service, configuration *roverlib.ServiceConfiguration)
 		}
 
 		for uint32(usedSlice) < (uint32(imgHeight)-1) && (longestConsecutive == nil) {
-			usedSlice += 5
+			usedSlice += 6
 
 			if int(usedSlice) >= imgHeight-1 {
 				break
